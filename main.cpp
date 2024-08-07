c098771f1ae49af2062d5895e0cf9fa88d2829b2
Denis Zmitrichenko <den.jmpr@gmail.com>
2024-08-07 18:32:45 +0500

    Completed "Uniform Variables".

--- main.cpp
+++ main.cpp
@@ -1,31 +1,39 @@
 #include <stdio.h>
 #include <string.h>
+#include <cmath>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 const GLint WIDTH = 800, HEIGHT = 600;
 
-GLuint VAO, VBO, shader;
+GLuint VAO, VBO, shader, uniformXMove;
 
-static const char* vShader = "										\n\
-#version 330														\n\
-																	\n\
-layout (location = 0) in vec3 pos;									\n\
-																	\n\
-void main()															\n\
-{																	\n\
-	gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);		\n\
+bool direction = true;
+float triOffset = 0.0f;
+float triMaxoffset = 0.7f;
+float triIncrement = 0.0005f;
+
+static const char* vShader = "													\n\
+#version 330																	\n\
+																				\n\
+layout (location = 0) in vec3 pos;												\n\
+																				\n\
+uniform float xMove;															\n\
+																				\n\
+void main()																		\n\
+{																				\n\
+	gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, pos.z, 1.0);			\n\
 }";
 
-static const char* fShader = "										\n\
-#version 330														\n\
-																	\n\
-out vec4 colour;													\n\
-																	\n\
-void main()															\n\
-{																	\n\
-	colour = vec4(1.0, 0.0, 0.0, 1.0);								\n\
+static const char* fShader = "													\n\
+#version 330																	\n\
+																				\n\
+out vec4 colour;																\n\
+																				\n\
+void main()																		\n\
+{																				\n\
+	colour = vec4(1.0, 0.0, 0.0, 1.0);											\n\
 }";
 
 void CreateTriangle()
@@ -111,6 +119,8 @@ void CompileShaders()
 		printf("Error validating program: '%s'\n", eLog);
 		return;
 	}
+
+	uniformXMove = glGetUniformLocation(shader, "xMove");
 }
 
 int main()
@@ -158,10 +168,26 @@ int main()
 	{
 		glfwPollEvents();
 
+		if (direction)
+		{
+			triOffset += triIncrement;
+		}
+		else
+		{
+			triOffset -= triIncrement;
+		}
+
+		if (abs(triOffset) >= triMaxoffset)
+		{
+			direction = !direction;
+		}
+
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shader);
+
+		glUniform1f(uniformXMove, triOffset);
 
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
