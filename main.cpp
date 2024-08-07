4cdee129226366f19d2d571b05ece65b93506b12
Denis Zmitrichenko <den.jmpr@gmail.com>
2024-08-07 20:42:10 +0500

    Completed "Transforming: Rotation".

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include <glm\gtc\type_ptr.hpp>
 
 const GLint WIDTH = 800, HEIGHT = 600;
+const float toRadians = 3.14159265f / 180.0f;
 
 GLuint VAO, VBO, shader, uniformModel;
 
@@ -17,6 +18,8 @@ bool direction = true;
 float triOffset = 0.0f;
 float triMaxoffset = 0.7f;
 float triIncrement = 0.0005f;
+
+float curAngle = 0.0f;
 
 static const char* vShader = "													\n\
 #version 330																	\n\
@@ -27,7 +30,7 @@ uniform mat4 model;																\n\
 																				\n\
 void main()																		\n\
 {																				\n\
-	gl_Position = model * vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);			\n\
+	gl_Position = model * vec4(pos.x * 0.4, pos.y * 0.4, pos.z, 1.0);			\n\
 }";
 
 static const char* fShader = "													\n\
@@ -186,13 +189,20 @@ int main()
 			direction = !direction;
 		}
 
+		curAngle += 0.01f;
+		if (curAngle >= 360)
+		{
+			curAngle -= 360;
+		}
+
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shader);
 
 		glm::mat4 model(1.0f);
-		model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
+		model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
 
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
