4d8eaae8ed6210ed14653faca4567d5bb7b33519
Denis Zmitrichenko <den.jmpr@gmail.com>
2024-08-08 21:57:04 +0500

    Completed "Clean Up".

---
+++ Window.cpp
@@ -0,0 +1,60 @@
+#include "Window.h"
+
+Window::Window()
+{
+	width = 800;
+	height = 600;
+}
+
+Window::Window(GLint windowWidth, GLint windowHeight)
+{
+	width = windowWidth;
+	height = windowHeight;
+}
+
+int Window::Initialise()
+{
+	if (!glfwInit())
+	{
+		printf("GLFW initialization failed!");
+		glfwTerminate();
+		return 1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	mainWindow = glfwCreateWindow(width, height, "Test Window", NULL, NULL);
+	if (!mainWindow)
+	{
+		printf("GLFW window creation failed!");
+		glfwTerminate();
+		return 1;
+	}
+
+	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+	glfwMakeContextCurrent(mainWindow);
+
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK)
+	{
+		printf("GLEW initialization failed!");
+		glfwDestroyWindow(mainWindow);
+		glfwTerminate();
+		return 1;
+	}
+
+	glEnable(GL_DEPTH_TEST);
+
+	glViewport(0, 0, bufferWidth, bufferHeight);
+}
+
+Window::~Window()
+{
+	glfwDestroyWindow(mainWindow);
+	glfwTerminate();
+}
