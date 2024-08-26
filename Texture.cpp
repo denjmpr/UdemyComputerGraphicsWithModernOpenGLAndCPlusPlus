bca0ce11911e59d5c69b9eef8419ef8a1a6c24f9
Denis Zmitrichenko <den.jmpr@gmail.com>
2024-08-26 20:15:29 +0500

    Completed "Directional Shadow Maps".

--- Texture.cpp
+++ Texture.cpp
@@ -29,7 +29,6 @@ bool Texture::LoadTexture()
 
 	glGenTextures(1, &textureID);
 	glBindTexture(GL_TEXTURE_2D, textureID);
-
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
